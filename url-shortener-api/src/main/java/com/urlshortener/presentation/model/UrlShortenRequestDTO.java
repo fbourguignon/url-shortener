03ed66bcdcb9efc8d81059301f26e3482780e1cf
Franklin Bourguignon <franklin_bourguignon@hotmail.com>
2022-11-13 22:50:31 -0300

    refact(adding url regex pattern validation)

--- url-shortener-api/src/main/java/com/urlshortener/presentation/model/UrlShortenRequestDTO.java
+++ url-shortener-api/src/main/java/com/urlshortener/presentation/model/UrlShortenRequestDTO.java
@@ -3,8 +3,14 @@ package com.urlshortener.presentation.model;
 import io.micronaut.core.annotation.Introspected;
 import lombok.Value;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 @Value
 @Introspected
 public class UrlShortenRequestDTO {
+
+    @NotNull
+    @Pattern(regexp = "(https?://|www\\\\.)[-a-zA-Z0-9+&@#/%?=~_|!:.;]*[-a-zA-Z0-9+&@#/%=~_|]", message = "Field must be a valid url.")
     private String url;
 }
